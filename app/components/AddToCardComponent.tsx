c97c417d1bd9bfc5a0a2a360195c7eac2659695d
neda-jahadi <neda_jahadi@yahoo.com>
2024-10-08 20:18:59 +0200

    First components

---
+++ app/components/AddToCardComponent.tsx
@@ -0,0 +1,12 @@
+'use client';
+import React from 'react'
+
+const AddToCardComponent = () => {
+  return (
+    <div>
+        <button onClick={() => console.log('clicked')}>Add to card</button>
+    </div>
+  )
+}
+
+export default AddToCardComponent
---
+++ app/components/CardComponent.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AddToCardComponent from './AddToCardComponent';
+
+const CardComponent = () => {
+  return (
+    <div>
+        <AddToCardComponent />
+    </div>
+  )
+}
+
+export default CardComponent
