f1b7041571694b18b2ab12a09ca3eb18eb721977
neda-jahadi <neda_jahadi@yahoo.com>
2024-10-08 20:41:14 +0200

    Activate data catching by fetch request

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
 import Link from "next/link";
 import CardComponent from "./components/CardComponent";
 
 export default function Home() {
   return (
     <main>
-      <h1>Hello world</h1>
+      <h1>Titan 1</h1>
       <Link href="/users">Users</Link>
       <CardComponent />
     </main>
