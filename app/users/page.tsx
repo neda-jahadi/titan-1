edd43d6518dfab980fbee2487fb54ed541975382
neda-jahadi <neda_jahadi@yahoo.com>
2024-10-19 10:42:33 +0200

    Access route parameters

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,35 +1,12 @@
 import React from 'react';
-interface User {
-    id: number;
-    name: string;
-    email: string
-}
-const UsersPage = async () => {
-    const res = await fetch (
-      'https://jsonplaceholder.typicode.com/users',
-      {next: {revalidate: 10}}
-    );
+import UserTable from './UserTable';
 
-    const users : User[] = await res.json();
+const UsersPage = () => {
+    
   return (
     <div>
         <h1>Users</h1>
-        <table className='table'>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Email</th>
-            </tr>
-          </thead>
-          <tbody>
-            {users.map(user => 
-              <tr key={user.id}>
-                <td>{user.name}</td>
-                <td>{user.email}</td>
-              </tr>
-             )}
-          </tbody>
-        </table>
+        <UserTable />
     </div>
   )
 }
