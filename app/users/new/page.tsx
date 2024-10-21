81e7dc4846de13009eb2de9bc387d44a05fa8302
neda-jahadi <neda_jahadi@yahoo.com>
2024-10-21 22:04:16 +0200

    Programmatic navigation, adding navigation by click event

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,8 +1,14 @@
+'use client'
 import React from 'react'
+import { useRouter } from 'next/navigation';
 
 function NewUserPage() {
+  const router = useRouter();
+
   return (
-    <div>NewUserPage</div>
+    <button
+      onClick={() => router.push('/users')}
+      className='btn btn-primary'>Create</button>
   )
 }
 
